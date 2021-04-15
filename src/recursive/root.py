95e7867545a036a035f8518cec40ca089e200c44
Saif Ul Islam <saifulislam84210@gmail.com>
2021-04-15 17:19:36 +0500

    changed TCP connections to UDP connections

--- src/recursive/root.py
+++ src/recursive/root.py
@@ -10,7 +10,7 @@ load_dotenv()  # take environment variables from .env
 # Unable to find a way to do relative imports
 # Resorting to prepanding the PYTHON PATH ENV
 # with the path where the module exists
-sys.path.insert(1, str(os.environ['MODEL_PATH']))
+sys.path.insert(1, os.path.abspath('.') + str(os.environ['MODEL_PATH']))
 
 from DNSMessageCacheHandler import DNSMessageCacheHandler
 # pylint: enable=no-member
@@ -27,17 +27,13 @@ for key in data:
     if domain not in topLevelDomains:
         topLevelDomains.append(domain)
 
-serv=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serv.bind((str(os.environ['HOSTNAME']),
            int(os.environ['ROOT_DOMAIN_PORT'])))
 
-serv.listen(5)
-
 while 1:
-    conn, addr=serv.accept()
-
-    data=str(conn.recv(int(os.environ['BYTES_TO_RECEIVE'])))
+    data, addr = serv.recvfrom(int(os.environ['BYTES_TO_RECEIVE']))
 
     if not data:
         break
@@ -89,7 +85,7 @@ while 1:
         The complete domain name - the value of the hostname
         For example, `google.com`
         '''
-        client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client.connect((str(os.environ['HOSTNAME']), int(
             os.environ['TOP_LEVEL_DOMAIN_PORT'])))
 
@@ -119,12 +115,11 @@ while 1:
         print('In Root - before sending to client')
         print(ProtocolMessage.__dict__)
 
-        conn.send(ProtocolMessage.EncodeObject())
+        serv.sendto(ProtocolMessage.EncodeObject(), addr)
     else:
         if DEBUG_MODE == 'ON':
             print('----\nIn Debug!')
             print(
                 f"\"{ProtocolMessage.GetStrForWebsite()}\", \"{ProtocolMessage.GetStrForWebsiteHost()}\", \"{topLevelDomains}\"")
-        conn.send(b"[ROOT_LEVEL_DOMAIN] 400: Bad request")
-    conn.close()
+        serv.sendto(b"[ROOT_LEVEL_DOMAIN] 400: Bad request", addr)
     print('Local DNS disconnected')
--- src/recursive/top.py
+++ src/recursive/top.py
@@ -10,7 +10,7 @@ load_dotenv()  # take environment variables from .env
 # Unable to find a way to do relative imports
 # Resorting to prepanding the PYTHON PATH ENV
 # with the path where the module exists
-sys.path.insert(1, str(os.environ['MODEL_PATH']))
+sys.path.insert(1, os.path.abspath('.') + str(os.environ['MODEL_PATH']))
 
 from DNSMessageCacheHandler import DNSMessageCacheHandler
 # pylint: enable=no-member
@@ -27,20 +27,13 @@ for key in JSONdata:
     if hostName not in hostNames:
         hostNames.append(hostName)
 
-serv=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serv.bind((str(os.environ['HOSTNAME']),
            int(os.environ['TOP_LEVEL_DOMAIN_PORT'])))
 
-serv.listen(5)
-
 while 1:
-    conn, addr=serv.accept()
-
-    # # Great client
-    # conn.send(b"I am TOP LEVEL DOMAIN")
-
-    data=str(conn.recv(int(os.environ['BYTES_TO_RECEIVE'])))
+    data, addr = serv.recvfrom(int(os.environ['BYTES_TO_RECEIVE']))
 
     if not data:
         break
@@ -86,7 +79,7 @@ while 1:
         '''
         Establishing a connection with the ALD as a client
         '''
-        client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client.connect((str(os.environ['HOSTNAME']), int(
             os.environ['AUTHORITATIVE_DOMAIN_PORT'])))
 
@@ -113,12 +106,11 @@ while 1:
         # # # Convert to dict
         ProtocolMessage=DNSMessageCacheHandler(
             **json.loads(f"{eval(response[1:])}"))
-        conn.send(ProtocolMessage.EncodeObject())
+        serv.sendto(ProtocolMessage.EncodeObject(), addr)
     else:
         if DEBUG_MODE == 'ON':
             print('----\nIn Debug!')
             print(
-                f"\"{ProtocolMessage.GetStrForWebsite()}\", \"{ProtocolMessage.GetStrForWebsiteHost()}\", \"{topLevelDomains}\"")
-        conn.send(b"[ROOT_LEVEL_DOMAIN] 400: Bad request")
-    conn.close()
+                f"\"{ProtocolMessage.GetStrForWebsite()}\", \"{ProtocolMessage.GetStrForWebsiteHost()}\", \"{hostNames}\"")
+        serv.sendto(b"[ROOT_LEVEL_DOMAIN] 400: Bad request", addr)
     print('ROOT LEVEL DOMAIN disconnected')
